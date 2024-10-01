175020fbf11bf80b647b499f85600e9d166d1ea8
Dot1298 <148091241+Dot1298@users.noreply.github.com>
2024-09-30 20:36:01 -0400

    Created an Array.h with all duplicate code from Ordered/UnorderedArray.h

--- Array.h
+++ Array.h
@@ -149,4 +149,5 @@ protected:
     int m_maxSize; // Maximum size of the array
     int m_growSize; // Amount the array can grow through expansion
     int m_numElements; // Number of items currently in my array
+
 };
